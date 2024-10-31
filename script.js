2601609c3db77e3c2314b2fcb2563e602cecbb2c
K.Durga Bhavani <bhavanidurga759@gmail.com>
2024-10-31 21:37:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+document.querySelector("#grandParent").addEventListener(
+  "click",
+  (e) => {
+    console.log("grand");
+    // e.stopPropagation();  //its stope tigiring only pass value not go another value
+  },
+  true
+// false
+);
+document.querySelector("#parent").addEventListener(
+  "click",
+  () => {
+    console.log("parent");
+    e.stopPropagation();
+  },
+//   false
+ true
+);
+document.querySelector("#child").addEventListener(
+  "click",
+  (e) => {
+    console.log("child");
+    // e.stopPropagation()
+  },
+//   true
+false
+
+);
+
+
